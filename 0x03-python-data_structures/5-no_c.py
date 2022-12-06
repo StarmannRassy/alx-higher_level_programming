d5e77de76ab544b17d4d4c113eb32bc0e02f4c25
StarmannRassy <boronicle@gmail.com>
2022-12-06 01:22:55 +0000

    tasks 5 ver 2.o updated solutions

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     updated_str = ''
     for i in my_string:
-        if i != 'c' and i != 'c':
+        if i != 'c' and i != 'C':
             updated_str += i
     return (updated_str)
